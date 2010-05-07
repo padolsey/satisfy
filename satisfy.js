2323747e850cafc39accd7fdffe109a5212d40ac
James Padolsey <jamespadolsey@gmail.com>
2010-05-07 16:17:45 +0100

    Fixed indentation

--- satisfy.js
+++ satisfy.js
@@ -21,8 +21,8 @@ var satisfy = (function(){
             NAME: /\[name=['"]*((?:[\w\u00c0-\uFFFF_-]|\\.)+)['"]*\]/,
             ATTR: /\[\s*((?:[\w\u00c0-\uFFFF_-]|\\.)+)\s*(?:(\S?=)\s*(['"]*)(.*?)\3|)\s*\]/g,
             TAG: /^((?:[\w\u00c0-\uFFFF\*_-]|\\.)+)/,
-			CLONE: /\:(\d+)(?=$|[:[])/,
-			COMBINATOR: /^[>~+]$/
+            CLONE: /\:(\d+)(?=$|[:[])/,
+            COMBINATOR: /^[>~+]$/
         },
         exprCallback = {
             ID: function(match, node) {
@@ -47,31 +47,31 @@ var satisfy = (function(){
         
         var tag = exprRegex.TAG.exec(part),
             node = document.createElement( tag && tag[1] !== '*' ? tag[1] : 'div' ),
-			fragment = document.createDocumentFragment(),
+            fragment = document.createDocumentFragment(),
             match, expr, regex, callback;
         
         for (expr in exprCallback) {
-			
-			regex = exprRegex[expr];
-			callback = exprCallback[expr];
-	    
-			if (exprCallback.hasOwnProperty(expr)) {
-				
-				if (regex.global) {
-					
-					while ( (match = regex.exec(part)) !== null ) {
-						callback( match, node );
-					}
-					
-					continue;
-					
-				}
-				
-				if (match = regex.exec(part)) {
-					callback( match, node );
-				}
-			
-			}
+            
+            regex = exprRegex[expr];
+            callback = exprCallback[expr];
+        
+            if (exprCallback.hasOwnProperty(expr)) {
+                
+                if (regex.global) {
+                    
+                    while ( (match = regex.exec(part)) !== null ) {
+                        callback( match, node );
+                    }
+                    
+                    continue;
+                    
+                }
+                
+                if (match = regex.exec(part)) {
+                    callback( match, node );
+                }
+            
+            }
             
         }
         
@@ -84,8 +84,8 @@ var satisfy = (function(){
     }
     
     function multiAppend(parents, children) {
-		
-		parents = parents.childNodes;
+        
+        parents = parents.childNodes;
         
         var pLen = parents.length,
             i = -1,
@@ -100,74 +100,74 @@ var satisfy = (function(){
                 parent.appendChild(parent = document.createElement('tbody'));
             }
             
-			parent.appendChild(children.cloneNode(true));
+            parent.appendChild(children.cloneNode(true));
             
         }
         
     }
     
     function satisfy(selector) {
-		
-		if (selector in satisfy.cache) {
-			return satisfy.cache[selector].cloneNode(true).childNodes;
-		}
+        
+        if (selector in satisfy.cache) {
+            return satisfy.cache[selector].cloneNode(true).childNodes;
+        }
         
         var selectorParts = [],
             fragment = document.createDocumentFragment(),
-			children,
-			prevChildren,
+            children,
+            prevChildren,
             curSelector,
             nClones = 1,
-			isSibling = false;
+            isSibling = false;
         
-		while ( (m = chunker.exec(selector)) !== null ) {
-			selectorParts.push(m[1]);
-		}
-		
-		// We're going in reverse
-		while (curSelector = selectorParts.pop()) {
-			
-			if (exprRegex.COMBINATOR.test(curSelector)) {
-				isSibling = curSelector === '~' || curSelector === '+';
-				continue;
-			}
-			
-			// Number of clones must be an int >= 1
-			nClones = ~~(curSelector.match(exprRegex.CLONE)||[,1])[1];
-			
-			prevChildren = children;
-			children = create(curSelector, nClones);
-			
-			if (prevChildren) {
-				
-				if (isSibling) {
-					children.appendChild(prevChildren);
-					isSibling = false;
-				} else {
-					multiAppend(children, prevChildren);
-				}
-				
-			}
-			
-		}
-		
-		fragment.appendChild(children);
-		
-		satisfy.cache[selector] = fragment.cloneNode(true);
+        while ( (m = chunker.exec(selector)) !== null ) {
+            selectorParts.push(m[1]);
+        }
+        
+        // We're going in reverse
+        while (curSelector = selectorParts.pop()) {
+            
+            if (exprRegex.COMBINATOR.test(curSelector)) {
+                isSibling = curSelector === '~' || curSelector === '+';
+                continue;
+            }
+            
+            // Number of clones must be an int >= 1
+            nClones = ~~(curSelector.match(exprRegex.CLONE)||[,1])[1];
+            
+            prevChildren = children;
+            children = create(curSelector, nClones);
+            
+            if (prevChildren) {
+                
+                if (isSibling) {
+                    children.appendChild(prevChildren);
+                    isSibling = false;
+                } else {
+                    multiAppend(children, prevChildren);
+                }
+                
+            }
+            
+        }
+        
+        fragment.appendChild(children);
+        
+        satisfy.cache[selector] = fragment.cloneNode(true);
         
         return fragment.childNodes;
         
     }
-	
-	satisfy.cache = {};
-	satisfy.create = create;
+    
+    satisfy.cache = {};
+    satisfy.create = create;
     
     if (window.jQuery !== undefined && jQuery.fn) {
-		
+        
         jQuery.satisfy = function(selector) {
             return jQuery(satisfy(selector));
         };
-		
+        
     }
     
     return satisfy;
